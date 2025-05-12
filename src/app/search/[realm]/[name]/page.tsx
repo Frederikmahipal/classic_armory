3d1d9e5923e85e0db640348cedc9886cf1e20188
Fred <frederikmhv@gmail.com>
2025-05-12 21:43:29 +0200

    edit

--- src/app/search/[realm]/[name]/page.tsx
+++ src/app/search/[realm]/[name]/page.tsx
@@ -93,6 +93,7 @@ export default async function SearchResults({
                         </div>
                       </div>
                     </Link>
+                    
                   </li>
                 ))}
               </ul>
